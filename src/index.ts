1ad14ef0e88948d0c7154abbca0ee7fa7ea086d7
Harsha <vhrsha11@gmail.com>
2021-08-11 15:36:10 -0500

    Updated graphql/mikroorm

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
-console.log("hello world");
+import {MikroORM} from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+//import { Post } from "./entities/Post";
+import config from './mikro-orm.config';
+import express from 'express'
+import {ApolloServer} from 'apollo-server-express';
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+
+const main = async () => {
+    const orm = await MikroORM.init(config);
+    await orm.getMigrator().up();
+
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false
+        })
+    });
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app});
+
+    app.listen(4000, () => {
+            console.log('server started on localhost:4000');
+    });
+};
+
+main().catch((err) =>  {
+    console.error(err);
+});
