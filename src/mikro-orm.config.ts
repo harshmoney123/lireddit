c418feaa0a471794732f55af3052581692f91a83
Harsha <vhrsha11@gmail.com>
2021-08-23 02:37:41 -0500

    Next.js + Charka Initalized

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -5,14 +5,14 @@ import path from "path";
 import { User } from "./entities/User";
 
 export default {
-    migrations: {
-        path: path.join(__dirname,"./migrations"), // path to the folder with migrations
-        pattern: /^[\w-]+\d+\.[tj]s$/, // regex pattern for the migration files
-    },
-    entities: [Post, User],
-    dbName: "lireddit",
-    user: "postgres",
-    password: "postgres",
-    type: "postgresql",
-    debug: !__prod__,
-    } as Parameters<typeof MikroORM.init>[0];
+  migrations: {
+    path: path.join(__dirname, "./migrations"), // path to the folder with migrations
+    pattern: /^[\w-]+\d+\.[tj]s$/, // regex pattern for the migration files
+  },
+  entities: [Post, User],
+  dbName: "lireddit",
+  user: "postgres",
+  password: "postgres",
+  type: "postgresql",
+  debug: !__prod__,
+} as Parameters<typeof MikroORM.init>[0];
--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,9 @@
 import { EntityManager, IDatabaseDriver, Connection } from "@mikro-orm/core";
-import { Request, Response} from "express";
-import { Session } from 'express-session';
+import { Request, Response } from "express";
+import { Session } from "express-session";
 
 export type MyContext = {
-    em: EntityManager<IDatabaseDriver<Connection>>;
-    req: Request & {session: Session};
-    res: Response;
-}
+  em: EntityManager<IDatabaseDriver<Connection>>;
+  req: Request & { session: Session };
+  res: Response;
+};
